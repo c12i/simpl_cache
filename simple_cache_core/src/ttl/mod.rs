fbba2da505c95632c98411936955684aba28b4f9
Collins Muriuki <hello@collinsmuriuki.xyz>
2023-03-23 11:32:11 +0300

    add ttl cache test cases

--- simple_cache_core/src/ttl/mod.rs
+++ simple_cache_core/src/ttl/mod.rs
@@ -19,33 +19,62 @@ where
 				map: map.clone(),
 				ttl,
 			};
-			
 			let thread_values = map.clone();
-
 			std::thread::spawn(move || loop {
 				thread_values.lock().unwrap().retain(|_, (_, instant)| {
-					instant.elapsed() < ttl.clone()
+					instant.elapsed() < ttl
 				});
 			});
-
 			cache
     }
 
-    pub fn insert(&mut self, key: K, value: V) {
+    pub fn insert(&self, key: K, value: V) {
         let now = Instant::now();
         self.map.lock().unwrap().insert(key, (value, now));
     }
 
-    pub fn get(&self, key: &K) -> Option<V> {
+    pub fn get(&self, key: K) -> Option<V> {
         self.map
 						.lock()
 						.unwrap()
-            .get(key)
+            .get(&key)
             .filter(|(_, instant)| instant.elapsed() <= self.ttl)
             .map(|(value, _)| value.to_owned())
     }
 
-    pub fn remove(&mut self, key: &K) -> Option<V> {
-        self.map.lock().unwrap().remove(key).map(|(value, _)| value)
+    pub fn remove(&self, key: K) -> Option<V> {
+        self.map.lock().unwrap().remove(&key).map(|(value, _)| value)
     }
+}
+
+
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	use std::time::Duration;
+
+	#[test]
+	fn test_cache_insert() {
+		let cache = TtlCache::new(Duration::from_secs(5));
+		cache.insert("foo", "test");
+		assert_eq!(cache.get("foo"), Some("test"));
+	}
+
+ #[test]
+	fn test_cache_get_expired() {
+		let cache = TtlCache::new(Duration::from_secs(1));
+		cache.insert("foo", "test");
+		std::thread::sleep(Duration::from_secs(2));
+		assert_eq!(cache.get("foo"), None);
+	}
+
+ #[test]
+	fn test_cache_remove() {
+		let cache = TtlCache::new(Duration::from_secs(5));
+		cache.insert("foo", "test");
+		assert_eq!(cache.get("foo"), Some("test"));
+		cache.remove("foo");
+		assert_eq!(cache.get("foo"), None);
+	}
 }
