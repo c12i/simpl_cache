4a93c52816648b05e4eba3ceed2a91c14fc34386
Collins Muriuki <hello@collinsmuriuki.xyz>
2023-03-23 11:19:08 +0300

    ttl protoype

---
+++ simple_cache_core/src/ttl/mod.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+
+#[derive(Debug)]
+pub struct TtlCache<K, V> {
+    map: Arc<Mutex<HashMap<K, (V, Instant)>>>,
+    ttl: Duration,
+}
+
+impl<K, V> TtlCache<K, V>
+where
+    K: Eq + std::hash::Hash + Send + 'static,
+		V: Clone + Send + 'static,
+{
+    pub fn new(ttl: Duration) -> Self {
+			let map = Arc::new(Mutex::new(HashMap::new()));
+			let cache = TtlCache {
+				map: map.clone(),
+				ttl,
+			};
+			
+			let thread_values = map.clone();
+
+			std::thread::spawn(move || loop {
+				thread_values.lock().unwrap().retain(|_, (_, instant)| {
+					instant.elapsed() < ttl.clone()
+				});
+			});
+
+			cache
+    }
+
+    pub fn insert(&mut self, key: K, value: V) {
+        let now = Instant::now();
+        self.map.lock().unwrap().insert(key, (value, now));
+    }
+
+    pub fn get(&self, key: &K) -> Option<V> {
+        self.map
+						.lock()
+						.unwrap()
+            .get(key)
+            .filter(|(_, instant)| instant.elapsed() <= self.ttl)
+            .map(|(value, _)| value.to_owned())
+    }
+
+    pub fn remove(&mut self, key: &K) -> Option<V> {
+        self.map.lock().unwrap().remove(key).map(|(value, _)| value)
+    }
+}
