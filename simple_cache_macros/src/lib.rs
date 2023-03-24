f55c716f58b9fccaa12a615838cd05215b38df14
Collins Muriuki <hello@collinsmuriuki.xyz>
2023-03-24 09:56:06 +0300

    Add ttl cache proc macro

--- simple_cache_macros/src/lib.rs
+++ simple_cache_macros/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate proc_macro;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn, ReturnType};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_attribute]
+pub fn ttl_cache(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse the function signature and extract the return type
+    let function = parse_macro_input!(item as ItemFn);
+    let function_name = &function.sig.ident;
+    let function_args = &function.sig.inputs;
+    let function_body = &function.block;
+    let function_return_type = match &function.sig.output {
+        ReturnType::Type(_, ty) => ty.as_ref(),
+        ReturnType::Default => {
+            panic!("`ttl_cache` can only be applied to functions that return a value")
+        }
+    };
+    let ttl = attr
+        .to_string()
+        .parse::<u64>()
+        .expect("Invalid TTL argument");
+    let ttl_duration = quote! { std::time::Duration::from_secs(#ttl) };
+
+    // Generate the wrapped function
+    let output = quote! {
+        fn #function_name(#function_args) -> #function_return_type {
+            let cache = ::simple_cache_core::TtlCache::new(#ttl_duration);
+            let key = stringify!(#function_name);
+            if let Some(cached_result) = cache.get(&key) {
+                return cached_result;
+            }
+            fn cached_function(#function_args) -> #function_return_type {
+                #function_body
+            }
+            let result = cached_function(#function_args);
+            cache.insert(key, result.clone());
+            result
+        }
+    };
+    output.into()
 }
