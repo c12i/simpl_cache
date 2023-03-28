ba3f05685190b83c7346ea22b1617c8a68afff05
Collins Muriuki <hello@collinsmuriuki.xyz>
2023-03-28 07:53:23 +0300

    Fix imports

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,12 +1,14 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use proc_macro2::{Ident, Span};
+use proc_macro2::Span;
 use quote::quote;
 use syn::punctuated::Punctuated;
 use syn::token::Comma;
-use syn::{parse_macro_input, FnArg, ItemFn, LitInt, ReturnType, Type};
+use syn::{parse_macro_input, FnArg, Ident, ItemFn, LitInt, Pat, ReturnType, Type};
 
+/// This proc macro is designed to cache function calls with a
+/// time-to-live (TTL) duration.
 #[proc_macro_attribute]
 pub fn ttl_cache(attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the anotated function signature and extract various properties
@@ -16,11 +18,12 @@ pub fn ttl_cache(attr: TokenStream, item: TokenStream) -> TokenStream {
     let function_body = &function.block;
     let function_visibitly = &function.vis;
     let key = function_name.to_string();
-    let cached_function = Ident::new(&format!("_{}", &key), Span::call_site());
+    let cached_function = Ident::new(&format!("__{}", &key), Span::call_site());
     let static_var = Ident::new(&key.to_ascii_uppercase(), Span::call_site());
     let function_return_type = get_function_return_type(&function.sig.output);
-    let ttl = parse_macro_input!(attr as LitInt);
-    let ttl = ttl.base10_parse::<u64>().expect("Invalid ttl argument");
+    let ttl = parse_macro_input!(attr as LitInt)
+        .base10_parse::<u64>()
+        .expect("Invalid ttl argument");
     // Extract variable names from function arguments
     let (function_args_names, function_arg_values) = get_function_args(function_args);
     // Generate the key from function name and arg values as token stream
@@ -48,7 +51,7 @@ pub fn ttl_cache(attr: TokenStream, item: TokenStream) -> TokenStream {
 fn get_function_return_type(output: &ReturnType) -> &Type {
     match output {
         ReturnType::Type(_, ty) => {
-            if let syn::Type::Tuple(ty) = &**ty {
+            if let Type::Tuple(ty) = &**ty {
                 // prevent #[ttl_cache] from getting applied to functions that explicitly
                 // return a unit type: `()`
                 if ty.elems.len() == 0 {
@@ -69,8 +72,8 @@ fn get_function_args(
     let names = args
         .iter()
         .filter_map(|arg| {
-            if let syn::FnArg::Typed(pat_type) = arg {
-                if let syn::Pat::Ident(arg_name) = &*pat_type.pat {
+            if let FnArg::Typed(pat_type) = arg {
+                if let Pat::Ident(arg_name) = &*pat_type.pat {
                     Some(arg_name.ident.clone())
                 } else {
                     None
@@ -83,8 +86,8 @@ fn get_function_args(
     let values = args
         .iter()
         .filter_map(|arg| {
-            if let syn::FnArg::Typed(pat_type) = arg {
-                if let syn::Pat::Ident(arg_ident) = &*pat_type.pat {
+            if let FnArg::Typed(pat_type) = arg {
+                if let Pat::Ident(arg_ident) = &*pat_type.pat {
                     Some(quote! { #arg_ident })
                 } else {
                     None
