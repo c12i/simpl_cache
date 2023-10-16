4cb0799d0367b3f621f1bb051b01396e55447953
Collins Muriuki <hello@collinsmuriuki.xyz>
2023-10-16 14:17:07 +0300

    Use internal crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 #![doc = include_str!("../README.md")]
 
-extern crate futures;
-extern crate simple_cache_core;
-extern crate simple_cache_macros;
-
 pub use futures::executor::block_on;
 pub use simple_cache_core::TtlCache;
 pub use simple_cache_macros::ttl_cache;
